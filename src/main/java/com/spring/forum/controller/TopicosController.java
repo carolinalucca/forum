0e217c038b033bd950b2a23ec18e18987e7cedba
Carolina Lucca <carolinaplucca@gmail.com>
2020-03-27 19:00:37 -0300

    refatoraçao do topicoController para utilizaçao do topicoRepository

--- src/main/java/com/spring/forum/controller/TopicosController.java
+++ src/main/java/com/spring/forum/controller/TopicosController.java
@@ -1,22 +1,21 @@
 package com.spring.forum.controller;
 
 import com.spring.forum.dto.TopicoDto;
-import com.spring.forum.model.Curso;
-import com.spring.forum.model.Topico;
+import com.spring.forum.repository.TopicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class TopicosController {
 
+    @Autowired
+    private TopicoRepository topicoRepository;
+
     @RequestMapping("/topicos")
     public List<TopicoDto> lista(String curso) {
-
-        Topico topico = new Topico("teste", "teste", new Curso("teste", "teste"));
-
-        return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+        return TopicoDto.converter(topicoRepository.findAll());
     }
 }
